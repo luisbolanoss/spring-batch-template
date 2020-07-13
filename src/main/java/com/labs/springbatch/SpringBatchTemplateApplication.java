19564f3d4bc7ccf3119640c7f7cfeb34456925b5
lbolanoss <lbolanoss@psl.com.co>
2020-07-13 09:25:16 -0500

    01. Create a basic job

---
+++ src/main/java/com/labs/springbatch/SpringBatchTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.labs.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchTemplateApplication.class, args);
+	}
+
+}
